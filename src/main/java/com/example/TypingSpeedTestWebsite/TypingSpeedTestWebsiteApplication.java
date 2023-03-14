93ef4bdfa0b76b3539328c17e45734c0d503991a
Bossanova567 <mygoodcovers@gmail.com>
2023-03-14 18:15:38 +0200

    Modified to work with Azure App Service and Azure Database for MySQL.

--- src/main/java/com/example/TypingSpeedTestWebsite/TypingSpeedTestWebsiteApplication.java
+++ src/main/java/com/example/TypingSpeedTestWebsite/TypingSpeedTestWebsiteApplication.java
@@ -2,9 +2,17 @@ package com.example.TypingSpeedTestWebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.example.TypingSpeedTestWebsite.controllers", "com.example.TypingSpeedTestWebsite.services"})
 @SpringBootApplication
-public class TypingSpeedTestWebsiteApplication {
+public class TypingSpeedTestWebsiteApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TypingSpeedTestWebsiteApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TypingSpeedTestWebsiteApplication.class, args);
